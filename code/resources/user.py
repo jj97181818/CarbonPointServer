499c65bb6201c5e2b36c73ea11c6e3d1dd9eb9d4
jj97181818 <jj97181818@gmail.com>
2019-05-17 22:12:38 +0800

    Implement Usermodel using SQLAlchemy

--- code/resources/user.py
+++ code/resources/user.py
@@ -24,13 +24,8 @@ class UserRegister(Resource):
         if UserModel.find_by_username(data['username']):
             return {'message', 'The user already exists.'}, 400
 
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        query = "INSERT INTO users VALUES (NULL, ?, ?)"
-        cursor.execute(query, (data['username'], data['password']))
-
-        connection.commit()
-        connection.close()
+        user = UserModel(data['username'], data['password'])
+        user.save_to_db()
 
         return {'message': 'User creates successfully.'}, 201
+ 
